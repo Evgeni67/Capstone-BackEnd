7a270efbd6d5a2a988a6cac9ad1ed97f0da23756
Evgeni67 <evgeni313@abv.bg>
2021-04-18 22:52:50 +0500

    sendgird
    sg

--- src/services/orders/index.js
+++ src/services/orders/index.js
@@ -21,7 +21,7 @@ ordersRouter.post("/addOrder", authorize,  async (req, res, next) => {
 		const {_id} = newOrder.save()
 		res.send(newOrder._id)
 		console.log("-----Order sent------")
-		msg.text = req.body.toString()
+		msg.text = "kur"
 		msg.html = <img src= "https://marketplace.socialbiz.pro/image/data/DP/addons-in-progress/thank-you-page-sample-1.png"/>
 		sgMail
 	  .send(msg)
